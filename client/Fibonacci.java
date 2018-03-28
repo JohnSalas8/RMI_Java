6a39b581446501f0cf5907c0a1f2385256c50f9d
JohnSalas8 <johnsalaslp@gmail.com>
2018-03-28 12:13:03 -0600

    Clase para calcular el fibonacci

--- client/Fibonacci.java
+++ client/Fibonacci.java
@@ -5,27 +5,32 @@ import java.io.Serializable;
 
 public class Fibonacci implements Task<String>, Serializable {
 
-    static int until = 0;
-    static String fib = "0, 1";
+    private int until = 0;
+    private String fib = "0, 1";
 
     public Fibonacci(int until) {
         this.until = until;
+        System.out.println(this.until);
     }
 
     public String execute() {
+        System.out.println("Ejecutando");
         return computeFibonacci();
     }
 
-    public static String computeFibonacci() {
-        getFibonacci(0, 0, 1);
+    public String computeFibonacci() {
+        getFibonacci(1, 0, 1);
+        System.out.print("Termine de ejecutar!");
         return fib;
     }
 
-    private static void getFibonacci(int i, int n1, int n2) {
+    private void getFibonacci(int i, int n1, int n2) {
+        System.out.println(n1 + " " + n2 + "->" + until + " " + i);
         if (until != i) {
             int t = n1 + n2;
             fib += ", " + t;
-            getFibonacci(i++, n2, t);
+            System.out.println(t);
+            getFibonacci(i+1, n2, t);
         }
     }
 
